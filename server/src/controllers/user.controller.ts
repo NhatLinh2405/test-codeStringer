8409c4207a880ce039da56cebad907603fd5ac57
Nhat Linh <nhatlinh240501@gmail.com>
2024-05-12 02:30:47 +0700

    fix: bug not update jwt to db

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import bcryptjs from "bcryptjs";
 import { NextFunction, Request, Response } from "express";
 import { User } from "../models";
@@ -32,10 +33,20 @@ export async function createUser(req: Request, res: Response, next: NextFunction
 			refreshToken,
 		};
 
-		res.status(201).json(handleResponse(response, 200, "User created successfully"));
+		return res.status(201).json(handleResponse(response, 200, "User created successfully"));
 	} catch (error) {
 		next(error);
 	}
+}
+
+export async function saveUser(userId: string, accessToken: string, refreshToken: string) {
+	return await User.updateOne(
+		{ _id: userId },
+		{
+			accessToken,
+			refreshToken,
+		}
+	);
 }
 
 // sign in
@@ -48,27 +59,67 @@ export async function signIn(req: Request, res: Response, next: NextFunction) {
 		});
 
 		if (!user) {
-			res.status(404).json(handleError({ message: "User not found" }, 404));
+			return res.status(404).json(handleError({ message: "User not found" }, 404));
 		}
 
 		if (user && bcryptjs.compareSync(password, user.password)) {
 			const { accessToken, refreshToken } = createJWT(user._id, user.email, user.name);
 
-			res.status(200).json({
-				message: "User signed in successfully",
-				data: {
-					accessToken,
-					refreshToken,
-				},
-			});
+			await saveUser(user._id, accessToken, refreshToken);
+			const response = {
+				accessToken,
+				refreshToken,
+			};
+			return res.status(200).json(handleResponse(response, 200, "User signed in successfully"));
 		}
 
-		throw new Error("Wrong password");
+		return res.status(401).json(handleError({ message: "Wrong password" }, 401));
 	} catch (error) {
 		next(error);
 	}
 }
 
+// login with google
+export const loginWithGoogle = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const { email, name, picture, sub } = await axios
+			.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+				headers: {
+					Authorization: `Bearer ${req.body.tokenId}`,
+				},
+			})
+			.then((response) => response.data);
+
+		const user = await User.findOne({ email });
+		if (!user) {
+			const newUser = new User({
+				name,
+				email,
+				image: picture,
+				password: bcryptjs.hashSync(sub, 10),
+			});
+
+			const { accessToken, refreshToken } = createJWT(newUser._id, newUser.email, newUser.name);
+			newUser.accessToken = accessToken;
+			newUser.refreshToken = refreshToken;
+			await newUser.save();
+
+			return res
+				.status(201)
+				.json(handleResponse({ accessToken, refreshToken }, 201, "User created successfully"));
+		}
+		const { accessToken, refreshToken } = createJWT(user._id, user.email, user.name);
+
+		await saveUser(user._id, accessToken, refreshToken);
+		return res
+			.status(200)
+			.json(handleResponse({ accessToken, refreshToken }, 200, "User signed in successfully"));
+	} catch (error) {
+		next(error);
+	}
+};
+
+// refresh token
 export async function refreshToken(req: Request, res: Response, next: NextFunction) {
 	const { refreshToken } = req.body;
 
@@ -82,6 +133,8 @@ export async function refreshToken(req: Request, res: Response, next: NextFuncti
 		}
 
 		const { accessToken, refreshToken: newRefreshToken } = createJWT(user._id, user.email, user.name);
+
+		await saveUser(user._id, accessToken, newRefreshToken);
 
 		res.status(200).json({
 			message: "Token refreshed successfully",
--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,11 +1,18 @@
 import express from "express";
-import { createUser, getProfile, refreshToken, signIn } from "../controllers/user.controller";
+import {
+	createUser,
+	getProfile,
+	loginWithGoogle,
+	refreshToken,
+	signIn,
+} from "../controllers/user.controller";
 import { currentUser } from "../middlewares/auth";
 
 const Router = express.Router();
 
 Router.route("/sign-up").post(createUser);
 Router.route("/sign-in").post(signIn);
+Router.route("/login-google").post(loginWithGoogle);
 Router.route("/me").get(currentUser, getProfile);
 Router.route("/refresh-token").post(refreshToken);
 
