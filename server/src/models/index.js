040d9ab59189b3a5440fd98a9698d749f6c3fe60
Nhat Linh <nhatlinh240501@gmail.com>
2024-05-12 03:16:44 +0700

    fix: bug

--- server/src/models/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.User = void 0;
-var user_model_1 = __importDefault(require("./user.model"));
-exports.User = user_model_1.default;
-//# sourceMappingURL=index.js.map
