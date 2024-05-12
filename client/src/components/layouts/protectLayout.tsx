2cc56c57c6a9417b5b8988e277b1b3c7191dd680
Nhat Linh <nhatlinh240501@gmail.com>
2024-05-12 12:22:12 +0700

    style: change bg protect layout

--- client/src/components/layouts/protectLayout.tsx
+++ client/src/components/layouts/protectLayout.tsx
@@ -4,7 +4,7 @@ import { getItem } from '../../utils/localstoreHandle'
 export default function ProtectLayout() {
   const accessToken = getItem('accessToken')
   return accessToken ? (
-    <div className='min-h-dvh flex-center mx-4'>
+    <div className='min-h-dvh flex-center mx-4 bg-gradient-to-br from-[#0ea2d5] to-[#b1d4b0]'>
       <Outlet />
     </div>
   ) : (
